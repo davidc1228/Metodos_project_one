b8ae1c042ff0bfc9bf46391f57377177d61772fb
David Cardenas <jesusdavidcardenasbarreto@gmail.com>
2023-08-07 14:02:38 -0500

    Actividad 1  Metodos

---
+++ Actividad1.R
@@ -0,0 +1,24 @@
+install.packages("dplyr")
+library(paqueteMET)
+library(dplyr)  
+library(tidyverse)
+library(lubridate)
+library(modeest)
+learnr::run_tutorial("Tutorial101", "paqueteMET")  # carga Tutorial101
+
+
+df = CarreraLuz22
+View(df)
+glimpse(df[,5])
+View(df[,"timerun"])
+
+
+df[,"timerun"] = (round(df$timerun/60, 2))
+view(df$timerun)
+
+new_df <- mutate(df, timerun_minutes = round(timerun/60, 2))
+
+media = mean(df$timerun, na.rm = TRUE)
+media_desvest = mean(df$timerun, na.rm = TRUE, trim = 0,025)
+
+More_frecuency = mfv(df$timerun) 
